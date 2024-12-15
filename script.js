d2790ae713888f88af11ccae7d5e34699e7818cc
Ritik Rastogi <ritikrastogi911@gmail.com>
2024-12-15 18:22:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,42 @@
-function hamburg(){
-    const navbar = document.querySelector(".dropdown")
-    navbar.style.transform = "translateY(0px)"
+function hamburg() {
+    const navbar = document.querySelector(".dropdown");
+    navbar.style.transform = "translateY(0px)";
+    navbar.style.zIndex = "1000";
 }
 
-function cancel(){
-    const navbar = document.querySelector(".dropdown")
-    navbar.style.transform = "translateY(-500px)"
+function cancel() {
+    const navbar = document.querySelector(".dropdown");
+    navbar.style.transform = "translateY(-500px)";
 }
 
 // Typewriter Effect
+const texts = ["DEVELOPER", "DESIGNER", "PROGRAMMER"];
 
-const texts = [
-    "DEVELOPER",
-    "DESIGNER",
-    "PROGRAMMER"
-]
-
-let speed  =100;
+let speed = 100;
 const textElements = document.querySelector(".typewriter-text");
 
 let textIndex = 0;
-let charcterIndex = 0;
+let characterIndex = 0;
 
-function typeWriter(){
-    if (charcterIndex < texts[textIndex].length){
-        textElements.innerHTML += texts[textIndex].charAt(charcterIndex);
-        charcterIndex++;
-        setTimeout(typeWriter, speed); 
-    }
-    else{
-        setTimeout(eraseText, 1000)
+function typeWriter() {
+    if (characterIndex < texts[textIndex].length) {
+        textElements.innerHTML += texts[textIndex].charAt(characterIndex);
+        characterIndex++;
+        setTimeout(typeWriter, speed);
+    } else {
+        setTimeout(eraseText, 1000);
     }
 }
 
-function eraseText(){
-    if(textElements.innerHTML.length > 0){
-        textElements.innerHTML = textElements.innerHTML.slice(0,-1);
-        setTimeout(eraseText, 50)
-    }
-    else{
+function eraseText() {
+    if (textElements.innerHTML.length > 0) {
+        textElements.innerHTML = textElements.innerHTML.slice(0, -1);
+        setTimeout(eraseText, 50);
+    } else {
         textIndex = (textIndex + 1) % texts.length;
-        charcterIndex = 0;
-        setTimeout(typeWriter, 500)
+        characterIndex = 0;
+        setTimeout(typeWriter, 500);
     }
 }
 
-window.onload = typeWriter
+window.onload = typeWriter;
