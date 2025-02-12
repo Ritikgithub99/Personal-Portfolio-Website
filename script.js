c3d4160c6b8c86f512e58bf64f951d16aeb0a3b7
Ritik Rastogi <ritikrastogi911@gmail.com>
2025-02-12 20:46:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+function hamburg(){
+    const navbar = document.querySelector(".dropdown")
+    navbar.style.transform = "translateY(0px)"
+}
+function cancel(){
+    const navbar = document.querySelector(".dropdown")
+    navbar.style.transform = "translateY(-500px)"
+}
