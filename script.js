8c7313f9bba6531a5de73939da805aa42dbc8bae
Ritik Rastogi <ritikrastogi911@gmail.com>
2025-02-13 22:46:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ function cancel(){
     const navbar = document.querySelector(".dropdown")
     navbar.style.transform = "translateY(-500px)"
 }
+document.addEventListener("DOMContentLoaded", function () {
+    var typed = new Typed(".auto-type", {
+      strings: ["Developer", "Designer",],
+      typeSpeed: 150,
+      backSpeed: 150,
+      loop: true, // Ensures it runs infinitely
+    });
+  });
